d25b839ae667ef0805b32ce657371ef9e852846f
HACKERA316 <ademmurat1616@gmail.com>
2022-09-20 15:52:07 +0300

    update

--- start-linux.sh
+++ start-linux.sh
@@ -3,5 +3,8 @@ clear
 # Installer ablyLinuxVPN new name ablyVPN
 "
 git clone https://github.com/HACKERA316/ablylinuxVPN.git
-cd apps/ablylinuxVPN
-python3 server/app.py
+cd ablylinuxVPN
+cd server
+python3 app.py
+clear
+echo starting to server goto browser
