94fcb243f8869203f7a862eb1c566eeecb8826b1
HACKERA316 <ademmurat1616@gmail.com>
2022-09-20 16:23:21 +0300

    update

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 banner = """
 development  python file dont downloads
 """
+print(banner)
+
+vpn = input("vpn name #> ")
+
+if vpn == "us1":
+    print(vpn + " connecting")
+elif vpn == "us2":
+    print(vpn + " vpn connecting")
+else:
+    print("vpn connecting to vpn "+vpn)
