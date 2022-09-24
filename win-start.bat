7c5597416b6e551af7e7962ed8d06f146f19a48e
HACKERA316 <ademmurat1616@gmail.com>
2022-09-24 15:30:51 +0300

    Create win-start.bat

---
+++ win-start.bat
@@ -0,0 +1,8 @@
+clear
+# Installer ablyLinuxVPN new name ablyVPN
+echo unistalll...
+rm -r ablylinuxVPN
+echo installing...
+git clone https://github.com/HACKERA316/ablylinuxVPN.git
+clear
+cd ablylinuxVPN && python3 main.py
